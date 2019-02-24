0f3dd4ac7a1281446816c65c07ce816277026d08
taityo <taicho.nv@gmail.com>
2019-02-24 10:08:59 +0900

    client側で、driverからの顔の操作をpub/sub出来るようにした

--- src/cozmoMqttClient.py
+++ src/cozmoMqttClient.py
@@ -9,37 +9,46 @@ class CozmoClient:
     # instance valiable
     self.host = host
     self.port = port
-
+    
     # callback valiable
     self.callback_lift = None
+    self.callback_head = None
 
     ### Subscriber
     self.lift_sub = mqtt.Client()
+    self.head_sub = mqtt.Client()
    
     # callback function
     self.lift_sub.on_connect = self.on_connect_lift
     self.lift_sub.on_message = self.on_message_lift
-
+    self.head_sub.on_connect = self.on_connect_head
+    self.head_sub.on_message = self.on_message_head
+    
     ### Publisher
     self.lift_pub = mqtt.Client()
+    self.head_pub = mqtt.Client()
 
   def run(self):
     ### Subscriber
-
+    
     # connection
     self.lift_sub.connect_async(self.host, self.port, keepalive=60)
+    self.head_sub.connect_async(self.host, self.port, keepalive=60)
 
     # loop_start
     self.lift_sub.loop_start()
-
+    self.head_sub.loop_start()
+    
 
     ### Publisher
 
     # connection
     self.lift_pub.connect_async(self.host, self.port, keepalive=60)
+    self.head_pub.connect_async(self.host, self.port, keepalive=60)
 
     # loop_start
     self.lift_pub.loop_start()
+    self.head_pub.loop_start()
 
 
   ### Subscriber Callback Function
@@ -55,6 +64,17 @@ class CozmoClient:
     if self.callback_lift != None:
       self.callback_lift(lift_pos)
 
+  def on_connect_head(self, client, userdate, flags, respons_code):
+    self.head_sub.subscribe('/head_angle')
+    print('Connected head_sub !!')
+
+  def on_message_head(self, client, userdata, msg):
+    print('Subscribed head_sub !!')
+
+    head_angle = json.loads(msg.payload)
+    if self.callback_head != None:
+      self.callback_head(head_angle)
+
 
   ### Publisher Function
  
@@ -68,19 +88,23 @@ class CozmoClient:
     self.lift_pub.publish('/move_lift', json.dumps(move_lift))
     print('Publish lift_pub')
 
+  def publish_move_head(self, speed):
+    
+    move_head = {
+      'speed': speed
+    }
+    
+    # dict -> str on json & publish
+    self.head_pub.publish('/move_head', json.dumps(move_head))
+    print('Publish head_pub')
 
-num = 1
+
+num = -1
 i = 0
 
-def func(lift_pos):
-  global num
+def func(p):
 
-  if lift_pos['ratio'] >= 0.97:
-    num = -1
-  elif lift_pos['ratio'] <= 0.03:
-    num = 1
-
-  print(lift_pos)
+  print(p)
     
 
 if __name__ == '__main__':
@@ -89,15 +113,19 @@ if __name__ == '__main__':
   
   # callback function
   cozmo_client.callback_lift = func
+  cozmo_client.callback_head = func
 
 
   cozmo_client.run()
   while True:
+    cozmo_client.publish_move_head(i)
     cozmo_client.publish_move_lift(i)
     i = i + num
     print('i :' + str(i))
 
     time.sleep(0.05)
+
+
 
 
 
